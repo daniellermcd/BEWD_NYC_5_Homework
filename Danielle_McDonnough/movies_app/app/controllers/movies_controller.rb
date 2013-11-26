9578b30317f69825487b34a5d681e9bc5ea10a9d
Danielle McDonnough <daniellermcd@gmail.com>
2013-11-25 20:44:10 -0500

    refactoring and textarea styling

--- Danielle_McDonnough/movies_app/app/controllers/movies_controller.rb
+++ Danielle_McDonnough/movies_app/app/controllers/movies_controller.rb
@@ -1,10 +1,11 @@
 class MoviesController < ApplicationController
+  before_action :find_movie, only: [:show]
+
   def index
     @movies = Movie.all
   end
 
   def show
-    @movie = Movie.find params[:id]
   end
 
   def new
@@ -12,12 +13,21 @@ class MoviesController < ApplicationController
   end
 
   def create
-    safe_movie = params.require(:movie).permit(:title, :description, :year_released, :rating)
     @movie = Movie.create safe_movie
     if @movie.save
       redirect_to @movie, notice: "Your movie was successfully created"
     else
       render 'new'
     end
+  end
+
+  private
+
+  def find_movie
+    @movie = Movie.find params[:id]
+  end
+
+  def safe_movie
+    params.require(:movie).permit(:title, :description, :year_released, :rating)
   end
 end
