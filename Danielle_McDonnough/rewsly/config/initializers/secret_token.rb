cf859581f6088102c6b6684d4e590f223e0c41d6
Danielle McDonnough <daniellermcd@gmail.com>
2013-11-26 11:44:53 -0500

    Initial commit

---
+++ Danielle_McDonnough/rewsly/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rewsly::Application.config.secret_key_base = '32532ffc85c70ba4f1d835a6c296c4cdcf427452492be6bc7595dc86a751dea7c680ae6c0dbf7130411142e1d8b4c8b55b2f78f14f91b704d524d645c589b99b'
