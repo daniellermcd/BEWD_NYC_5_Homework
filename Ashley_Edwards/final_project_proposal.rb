bede4be1de0955c73a1d184d07c898cd283a2147
Ashley Edwards <ashley.elize@gmail.com>
2013-12-02 18:24:47 -0500

    project proposal

---
+++ Ashley_Edwards/final_project_proposal.rb
@@ -0,0 +1,19 @@
+Overview: I want to build a web app that allows users to search for schools based on school budget information. 
+
+Why: I work in education and believe all stakeholders (teachers, administrators, students, parents, regulating bodies) should be aware of how schools are budgeting state/federal money to serve children. 
+
+Where: Because I could only find an API for Philadelphia school budgets, this app will only cover that area. API here -> http://phillyschools.info/
+
+The User Experince:
+1. User signs into site or uses FB log-in
+2. User can search for schools based on Total Spending per Pupil, Administrator Salaries, $ spent on Textbooks, etc.
+3. The search returns back schools that fall into that category, along with their full budget information.
+4. Users can leave comments on the school budgets as they view them.
+
+Some features that might be cool:
+* Letting users leave comments with the option to post on Facebook or Twitter
+* Allowing users to make comparisons of schools
+* Allowing users to compare school budget info with school performance data
+* Option for users to email the school board of specific schools for questions regarding budgeting
+
+The idea is still taking shape, but this is what I am thinking so far. 
