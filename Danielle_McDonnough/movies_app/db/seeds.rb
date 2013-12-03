54461e81c404040dd3e8bbcd8dadda8b8cf714f3
Danielle McDonnough <daniellermcd@gmail.com>
2013-12-02 20:21:58 -0500

    update rails

--- Danielle_McDonnough/movies_app/db/seeds.rb
+++ Danielle_McDonnough/movies_app/db/seeds.rb
@@ -5,121 +5,121 @@ Movie.create [
     title: "The Shawshank Redemption",
     description: "Morgan Freeman in Jail",
     year_released: 1994,
-    rating: nil
+    rating: 3
   },
   {
     title: "The Godfather",
     description: "Hard core Gangster activity",
     year_released: 1972,
-    rating: nil
+    rating: 3
   },
   {
     title: "The Godfather: Part II",
     description: "More Gangsters, aw yea",
     year_released: 1974,
-    rating: nil
+    rating: 3
   },
   {
     title: "Pulp Fiction",
     description: "Samuel Jackson screams a LOT! Say What again!",
     year_released: 1994,
-    rating: nil
+    rating: 3
   },
   {
     title: "The Good, the Bad and the Ugly",
     description: "Cowboys will be cowboys",
     year_released: 1966,
-    rating: nil
+    rating: 3
   },
   {
     title: "12 Angry Men",
     description: "Really old movie about a court case. They have anger issues",
     year_released: 1957,
-    rating: nil
+    rating: 3
   },
   {
     title: "The Dark Knight",
     description: "Batman (not a super hero), battles the joker for who can have the most not normal voice ever",
     year_released: 2008,
-    rating: nil
+    rating: 3
   },
   {
     title: "Schindler's List",
     description: "It's about the Holocaust I think",
     year_released: 1993,
-    rating: nil
+    rating: 3
   },
   {
     title: "The Lord of the Rings: The Return of the King",
     description: "Something to do with Hobbits",
     year_released: 2003,
-    rating: nil
+    rating: 3
   },
   {
     title: "Fight Club",
     description: "This is your life... Apparently it's really easy to make explosives",
     year_released: 1999,
-    rating: nil
+    rating: 3
   },
   {
     title: "Star Wars: Episode V - The Empire Strikes Back",
     description: "The best Star Wars EVER",
     year_released: 1980,
-    rating: nil
+    rating: 3
   },
   {
     title: "The Lord of the Rings: The Fellowship of the Ring",
     description: "More Hobbits",
     year_released: 2001,
-    rating: nil
+    rating: 3
   },
   {
     title: "One Flew Over the Cuckoo's Nest",
     description: "Crazy guy does crazy stuff",
     year_released: 1975,
-    rating: nil
+    rating: 3
   },
   {
     title: "Inception",
     description: "Dream in a dream, in a nap, in a dozing off in class",
     year_released: 2010,
-    rating: nil
+    rating: 3
   },
   {
     title: "Goodfellas",
     description: "I think Joe Pesci is in this one",
     year_released: 1990,
-    rating: nil
+    rating: 3
   },
   {
     title: "Star Wars",
     description: "Let the Wookie win",
     year_released: 1977,
-    rating: nil
+    rating: 3
   },
   {
     title: "Seven Samurai",
     description: "One of the best films ever made... according to Wikipedia",
     year_released: 1954,
-    rating: nil
+    rating: 3
   },
   {
     title: "Forrest Gump",
     description: "Life is like a box of chocolates. It will give you diabetes",
     year_released: 1994,
-    rating: nil
+    rating: 3
   },
   {
     title: "The Matrix",
     description: "Playing videogames can teach you kung-fu",
     year_released: 1999,
-    rating: nil
+    rating: 3
   },
   {
     title: "The Lord of the Rings: The Two Towers",
     description: "Again with the Hobbits and their jewelry obsession",
     year_released: 2002,
-    rating: nil
+    rating: 3
   }]
 
 puts "Finished adding seeds"
