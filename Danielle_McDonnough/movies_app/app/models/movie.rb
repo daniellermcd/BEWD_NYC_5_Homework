f05e555080a56967965aff98018e7d671609de83
Danielle McDonnough <daniellermcd@gmail.com>
2013-12-02 19:12:14 -0500

    removed duplicate numericality validation

--- Danielle_McDonnough/movies_app/app/models/movie.rb
+++ Danielle_McDonnough/movies_app/app/models/movie.rb
@@ -1,7 +1,6 @@
 class Movie < ActiveRecord::Base
   validates :title, :description, presence: true
   validates :description, length: { minimum: 10, message: "must be greater than 10 characters" }
-  validates :year_released, numericality: true
   validates :year_released, numericality: { greater_than: 0, message: "must be greater than 0" }
   validates :rating, numericality: true
   validates :rating, inclusion: { in: 1..5, message: "must be between 1 and 5" }
